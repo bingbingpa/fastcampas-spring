b4f71ac17e3a0b51e953d9c981b06f9c10904adb
shpark <bingbingpa@gmail.com>
2020-03-15 23:02:35 +0900

    servlet

--- src/main/java/com/bingbingpa/web/SimpleServlet.java
+++ src/main/java/com/bingbingpa/web/SimpleServlet.java
@@ -1,5 +1,18 @@
 package com.bingbingpa.web;
 
-public class SimpleServlet {
+import java.io.IOException;
+import java.io.PrintWriter;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SimpleServlet extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter writer = resp.getWriter();
+		writer.println("Hello World");
+	}
 }
