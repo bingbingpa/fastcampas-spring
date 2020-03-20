64d27fa8778e0ce790ccc58709e71c6a529cb020
shpark <bingbingpa@gmail.com>
2020-03-21 00:35:03 +0900

    connection 수정

--- src/main/java/com/bingbingpa/cli/ConnectionFactory.java
+++ src/main/java/com/bingbingpa/cli/ConnectionFactory.java
@@ -6,18 +6,27 @@ import java.sql.SQLException;
 
 public class ConnectionFactory {
 	
+	private String driverClass;
 	private String url;
 	private String user;
 	private String password;
 	
 	
-	public ConnectionFactory(String url, String user, String password) {
+	public ConnectionFactory(String driverClass, String url, String user, String password) {
+		this.driverClass = driverClass;
 		this.url = url;
 		this.user = user;
 		this.password = password;
 	}
 
+
 	public Connection createConnection() throws SQLException {
+		try {
+			Class.forName(this.driverClass);
+		} catch (ClassNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 		return DriverManager.getConnection(this.url, this.user, this.password); 
 	}
 }
--- src/main/java/com/bingbingpa/cli/Main.java
+++ src/main/java/com/bingbingpa/cli/Main.java
@@ -15,12 +15,7 @@ import lombok.extern.slf4j.Slf4j;
 public class Main {
 	public static void main(String[] args) throws SQLException {
 		log.info("Hello World!");
-		try {
-			Class.forName("org.h2.Driver");
-		} catch (ClassNotFoundException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
+		
 		ApplicationContext context = new ClassPathXmlApplicationContext("dao.xml");
 		Dao dao = context.getBean("dao", Dao.class);
 		dao.run();
