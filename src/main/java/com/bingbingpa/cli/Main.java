114af8e72fcc8b58d33d1c8f09f3f6450046578b
shpark <bingbingpa@gmail.com>
2020-03-22 02:44:41 +0900

    spring bean, configuration 학습

--- src/main/java/com/bingbingpa/cli/Main.java
+++ src/main/java/com/bingbingpa/cli/Main.java
@@ -3,6 +3,7 @@ package com.bingbingpa.cli;
 import java.sql.SQLException;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.Lifecycle;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import lombok.extern.slf4j.Slf4j;
@@ -17,6 +18,10 @@ public class Main {
 		log.info("Hello World!");
 		
 		ApplicationContext context = new ClassPathXmlApplicationContext("dao.xml");
+//		Lifecycle lifeCyle = context.getBean(Lifecycle.class);
+//		log.info(">>>>>>>>>1 : " + lifeCyle.isRunning());
+//		context.close();
+//		log.info(">>>>>>>>>2 : " + lifeCyle.isRunning());
 		Dao dao = context.getBean("dao", Dao.class);
 		dao.run();
 	}
